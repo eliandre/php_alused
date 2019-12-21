f0b3650dc3de1fe778586017efe035a71a67b715
Andre Eli <andre.eli@khk.ee>
2019-12-21 20:21:53 +0200

    Ümardamise lisamine.

--- vormid/ruumalad.php
+++ vormid/ruumalad.php
@@ -1,10 +1,10 @@
 <?php
 
-$raadius = $_GET['raadius'];
-$korgus = $_GET['korgus'];
+$r = $_GET['raadius'];
+$k = $_GET['korgus'];
 
-$kera = (4*pi()*$raadius**3)/3;
-$koonus = (pi()*$korgus*$raadius**2)/3;
-$silinder = pi()*$korgus*$raadius**2;
+$kera = round(((4*pi()*$r**3)/3),2);
+$koonus = round(((pi()*$k*$r**2)/3),2);
+$silinder = round((pi()*$k*$r**2),2);
 
-echo 'Sisestatud andmete põhjal on kera ruumala '.$kera.', koonuse ruumala '.$koonus.' ja silindri ruumala'.$silinder.'.';
+echo 'Sisestatud andmete põhjal on kera ruumala '.$kera.', koonuse ruumala '.$koonus.' ja silindri ruumala '.$silinder.'.';
