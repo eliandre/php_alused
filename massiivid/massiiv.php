afc5b422f24786ade1fa26785f89f28c824b3607
Andre Eli <andre.eli@khk.ee>
2020-01-10 11:18:23 +0200

    Massiivi loomine ja väljastamine.

---
+++ massiivid/massiiv.php
@@ -0,0 +1,22 @@
+<?php
+$vso19 = array(
+    'Anne-Mari',
+    'Andre',
+    'Rene',
+    'Martin',
+    'Marit',
+    'Erko',
+    'Kerli',
+    'Hanna-Liisa',
+    'Jaana'
+);
+
+echo $vso19[0].'<br>';
+echo $vso19[1].'<br>';
+
+$opilaste_arv = count($vso19);
+echo 'VSo19 rühmas on ' .$opilaste_arv. ' õpilast.<br>';
+echo 'Need on: <br>';
+for($i = 0; $i < $opilaste_arv; $i++){
+    echo $vso19[$i].'<br>';
+}
