fb8621b55fbfe0b283422a0be38667553062c6cd
Andre Eli <andre.eli@khk.ee>
2020-01-31 11:10:31 +0200

    Trimmimine ja strippimine.

--- tekstifunktsioonid/pikkus_karpimine.php
+++ tekstifunktsioonid/pikkus_karpimine.php
@@ -3,3 +3,21 @@
 $tekst = 'Experience is the teacher of fools';
 echo strlen($tekst).'<br>';
 echo str_word_count($tekst);
+echo '<hr>';
+
+$tekst = ' 	A woman should soften but not weaken a man   ';
+echo "<pre>$tekst</pre>";
+echo "<pre>".trim($tekst)."</pre>";
+echo "<pre>".ltrim($tekst)."</pre>";
+echo "<pre>".rtrim($tekst)."</pre>";
+
+$tekst = 'A woman should soften but not weaken a man';
+echo trim($tekst, "A, a, k..n, w");
+echo '<hr>';
+
+$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
+echo strip_tags($tekst);
+echo '<hr>';
+
+$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
+echo strip_tags($tekst, '<b>, <br>');
