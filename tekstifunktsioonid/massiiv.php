e6e277b98bb0ddbd17d227a50eae810d482d8058
Andre Eli <andre.eli@khk.ee>
2020-01-31 11:42:46 +0200

    Massiividega tegelemine.

--- tekstifunktsioonid/massiiv.php
+++ tekstifunktsioonid/massiiv.php
@@ -12,3 +12,12 @@ echo '<hr>';
 
 //sõnad kui massiivi elemendid
 print_r(str_word_count($tekst,1));
+echo '<br>';
+//sõna väljakutsumine
+$sona = str_word_count($tekst, 1);
+echo $sona[2];
+echo '<br>';
+
+//Andes str_word_count() funktsioonile parameetri 2, määratakse sõna indeks vastava sümboli indeksiga kogu massiivis.
+print_r(str_word_count($tekst, 2));
+
