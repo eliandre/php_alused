4a0245d68bce232f449a32ba0545f14ea492b130
Andre Eli <andre.eli@khk.ee>
2020-02-10 14:27:43 +0200

    Lugemine ridade kaupa.

--- tekstifailid/lugemine.php
+++ tekstifailid/lugemine.php
@@ -11,10 +11,22 @@ echo nl2br($faili_sisu);
 fclose($minu_fail);
 
 // faili lugemise lühem verisoon
-echo nl2br(file_get_contents($allikas)).'<hr>';
-
+// echo nl2br(file_get_contents($allikas)).'<hr>';
+echo '<hr>';
 // lugemine ridade kaupa
+$minu_fail = fopen($allikas, 'r');
 // loeb esimese rea
 $faili_sisu = fgets($minu_fail);
 echo $faili_sisu;
+fclose($minu_fail);
+echo '<hr>';
+
+// kõikide ridade kuvamine, feof() kontrollib, kas on jõutud faili lõppu
+$minu_fail = fopen($allikas, 'r');
+// kõikide ridade kuvamine
+while(!feof($minu_fail)){
+    $faili_sisu = fgets($minu_fail);
+    echo nl2br($faili_sisu);
+}
+
 fclose($minu_fail);
