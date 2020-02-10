1af0d42269efce024deb4ac71d7b26b21e4b4fb9
Andre Eli <andre.eli@khk.ee>
2020-02-10 13:39:23 +0200

    Failist lugemine ridade kaupa.

--- tekstifailid/lugemine.php
+++ tekstifailid/lugemine.php
@@ -6,5 +6,15 @@ $allikas = 'loomad.txt';
 $minu_fail = fopen($allikas, 'r') or die ("Faili pole");
 // faili sisu kuvamine
 $faili_sisu = fread($minu_fail, filesize($allikas));
+// et html suudaks reavahetusest \n aru saada
+echo nl2br($faili_sisu);
+fclose($minu_fail);
+
+// faili lugemise lühem verisoon
+echo nl2br(file_get_contents($allikas)).'<hr>';
+
+// lugemine ridade kaupa
+// loeb esimese rea
+$faili_sisu = fgets($minu_fail);
 echo $faili_sisu;
 fclose($minu_fail);
