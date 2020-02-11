2ca0949046b035ee86dae2deaf637f497450d797
Andre Eli <andre.eli@khk.ee>
2020-02-11 13:20:36 +0200

    Tsükli lisamine kõikide ridade kättesaamiseks.

--- csv/sisu_kuvamine.php
+++ csv/sisu_kuvamine.php
@@ -5,3 +5,19 @@ $minu_csv = fopen($allikas, 'r') or die ('Faili ei leitud');
 $rida = fgetcsv($minu_csv, filesize($allikas),';');
 var_dump($rida);
 fclose($minu_csv);
+echo '<hr>';
+
+// kõikide ridade kättesaamine
+$minu_csv = fopen($allikas, 'r') or die ('Faili ei leitud');
+$jrk = 1;
+while(!feof($minu_csv)){
+    $rida = fgetcsv($minu_csv, filesize($allikas), ';');
+    $arv = count($rida); // rea väljade arv
+    echo $jrk.'. '; // järjekorra number
+    $jrk++;
+    for($i = 0; $i < $arv; $i++){
+        echo $rida[$i].' ';
+    }
+    echo '<br>';
+}
+fclose($minu_csv);
