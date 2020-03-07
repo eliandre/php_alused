44864ec5735d5229652115fceef4a80ad1a4161c
Andre Eli <andre.eli@khk.ee>
2020-03-07 22:30:02 +0200

    Otsingu lisamine.

--- katsetamine/muusikapood.php
+++ katsetamine/muusikapood.php
@@ -27,3 +27,32 @@ while($rida = mysqli_fetch_assoc($valjund2)){
 mysqli_free_result($valjund2);
 mysqli_close($yhendus);
 ?>
+<hr>
+<form method="get" action="">
+    Otsing <input type="text" name="otsi">
+    <input type="submit" value="otsi">
+</form>
+<?php include('config.php');
+if(!empty($_GET['otsi'])){
+    // kasutaja tekst vormist
+    $otsi = $_GET['otsi'];
+    $otsi = htmlspecialchars(trim($otsi));
+    // päring
+    $paring3 = 'SELECT * FROM ALBUMID WHERE Artist LIKE "%'.$otsi.'%"';
+    $valjund3 = mysqli_query($yhendus, $paring3);
+    $tulemused= mysqli_num_rows($valjund3);
+
+    echo 'Otsingusõna: '.$otsi.'<br>';
+    echo 'Vastused: <br>';
+    if($tulemused == 0){
+        echo "Tulemusi ei leitud.";
+    }
+
+    while($rida = mysqli_fetch_assoc($valjund3)){
+        echo $rida['Artist'].' - '.$rida['Album'].' - '.$rida['Aasta'].'<br>';
+    }
+    mysqli_free_result($valjund3);
+    mysqli_close($yhendus);
+}
+?>
+
